7a6db8f8963e4d149952b6eb068caf568a50fe0c
Erick Vega <evega-12@hotmail.com>
2019-03-14 02:22:16 -0600

    Update SecuenciaLedsIinfrarrojo.ino

--- SecuenciaLedsIinfrarrojo.ino
+++ SecuenciaLedsIinfrarrojo.ino
@@ -4,6 +4,8 @@ int pin3=4;
 int pin4=5;
 
 int valor=0;
+bool led=false;
+bool inicio=false;
 
 int pin10=8;//Pines de salida a leds
 int pin11=9;
@@ -24,42 +26,75 @@ pinMode(pin10, OUTPUT);//Definir pines de salida para leds
 pinMode(pin11, OUTPUT);
 pinMode(pin12, OUTPUT);
 pinMode(pin13, OUTPUT);
+
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void repaso(){
+  delay(500);
+  digitalWrite(pin10,HIGH);
+  delay(500);
+  digitalWrite(pin11,HIGH);
+  delay(500);
+  digitalWrite(pin12,HIGH);
+  delay(500);
+  digitalWrite(pin13,HIGH);
+  delay(500);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,LOW);
 }
 
 void loop() {
+  
+  if(!inicio){
+  repaso();
+  inicio=true;
+  }
+if(led==false) { 
+digitalWrite(LED_BUILTIN, HIGH);
+led=true;
+}
+else{
+  digitalWrite(LED_BUILTIN, LOW);
+  led=false;
+}
+
+
   // put your main code here, to run repeatedly:
-  valor = digitalRead(pin1);
-if(valor ==1){//Si se detecta señal en sensor 1
+  
+if(digitalRead(pin1) ==HIGH){//Si se corta señal en sensor 1
   
  digitalWrite(pin10,HIGH);
-  //pin10 = 1;//se prende la priner luz
+ digitalWrite(LED_BUILTIN, HIGH);
+
+  //pin10 = 1;//se prende la primer luz
 }
-  valor = digitalRead(pin2);
-  valor = digitalRead(pin1);
-if(pin2==1&&pin1==0){//si se detecta señal en el sensor 2 y ya no en el 1
+
+
+  if(digitalRead(pin2)==HIGH&&digitalRead(pin1)==LOW){//si se detecta señal en el sensor 2 y ya no en el 1
   digitalWrite(pin11,HIGH);
   digitalWrite(pin10,LOW);
   //pin11=1;//se prende la luz 2
   //pin10=0;//y se apaga la luz 1
 }
- valor = digitalRead(pin3);
-  valor = digitalRead(pin2);
-if(pin3==1&&pin2==0){
+
+if(digitalRead(pin3)==HIGH&&digitalRead(pin2)==LOW){
   digitalWrite(pin12,HIGH);
   digitalWrite(pin11,LOW);
  // pin12=1;
   //pin11=0;
 }
-valor = digitalRead(pin4);
-valor = digitalRead(pin3);
-if(pin4==1&&pin3==0){
+
+if(digitalRead(pin4)==HIGH&&digitalRead(pin3)==LOW){
   digitalWrite(pin13,HIGH);
   digitalWrite(pin12,LOW);
   //pin13=1;
   //pin12=0;
 }
-valor = digitalRead(pin4);
-if(pin4==0){//si ya no hay señal en el sensor 4
+
+if(digitalRead(pin4)==LOW){//si ya no hay señal en el sensor 4
   digitalWrite(pin13,LOW);
   //pin13=0;//se apaga la ultima luz
 }
